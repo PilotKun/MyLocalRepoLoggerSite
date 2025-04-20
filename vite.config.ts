69be3bb643c2a9569f414a179f1b892fd9f95d95
Pilot_Kun <keerthi.modi1910@gmail.com>
2025-04-20 23:54:47 +0530

    Fix4Vite: Fix

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || "/MyLocalRepoLoggerSite",
   plugins: [
     react(),
     runtimeErrorOverlay(),
